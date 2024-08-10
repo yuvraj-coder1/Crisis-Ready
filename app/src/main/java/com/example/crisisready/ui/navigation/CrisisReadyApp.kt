0f7f76a4700a80cf1ec683b73413028a9f3dfedf
yuvraj-coder1 <yuvrajshorewalavis@gmail.com>
2024-08-10 22:54:32 +0530

    bug fix

--- app/src/main/java/com/example/crisisready/ui/navigation/CrisisReadyApp.kt
+++ app/src/main/java/com/example/crisisready/ui/navigation/CrisisReadyApp.kt
@@ -23,10 +23,10 @@ fun CrisisReadyApp(modifier: Modifier = Modifier, userData: UserData?, onSignOut
     NavHost(navController = navController, startDestination = Home) {
         composable<Home> {
             HomeScreen(
-                onDoDontsClicked = {navController.navigate(DoDonts)},
-                onMapClicked = {navController.navigate(Map)}
-                onEmergencyContactClicked = {navController.navigate(Contact)},
-                onSafetyTipClicked = {navController.navigate(SafetyTips)}
+                onDoDontsClicked = { navController.navigate(DoDonts) },
+                onMapClicked = { navController.navigate(Map) },
+                onEmergencyContactClicked = { navController.navigate(Contact) },
+                onSafetyTipClicked = { navController.navigate(SafetyTips) }
             )
         }
         composable<DoDonts> {
@@ -38,9 +38,11 @@ fun CrisisReadyApp(modifier: Modifier = Modifier, userData: UserData?, onSignOut
         }
         composable<Map> {
             MapContent(url = "file:///android_asset/map.html")
+        }
         composable<Ward> {
             WardScreen()
         }
+
         composable<Contact> {
             ContactScreen()
         }
